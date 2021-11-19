d2d9142f2dd6a2c12932c7fe82053a5543a26237
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-11-19 12:37:24 +0200

    todoApp

---
+++ README.md
@@ -0,0 +1,20 @@
+# TODO_Project
+
+**[Site link]()**
+
+The tech stack is:
+
+- HTML5
+- CSS3
+- Flexbox
+- Sass (Scss)
+- BEM methodology
+- JavaScript
+- React
+- Redux
+
+#### Author
+
+- ##### Ilyass-Elarbaouti
+
+# TODO_Project
---
+++ src/tasks/components/TasksList.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Task from '../../Task';
+import CreateTaskInput from './CreateTaskInput.jsx';
+import * as tasksAction from '../tasks.actions';
+import { sortedTasksListSelector } from '../tasks.selectors';
+
+class TasksList extends React.Component {
+  componentDidMount() {
+    this.props.getTaskList();
+  }
+
+  render() {
+    return (
+      <main className="todo-list">
+        <CreateTaskInput onCreate={this.props.createTask} />
+        <ul className="list">
+          {this.props.tasks.map(task => (
+            <Task
+              key={task.id}
+              {...task}
+              onChange={this.props.updateTask}
+              onDelete={this.props.deleteTask}
+            />
+
+            // <Task id={task.id} text={task.text} done={task.done} />
+          ))}
+        </ul>
+      </main>
+    );
+  }
+}
+
+TasksList.propTypes = {
+  getTaskList: PropTypes.func.isRequired,
+  updateTask: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
+  createTask: PropTypes.func.isRequired,
+  tasks: PropTypes.arrayOf(PropTypes.shape()),
+};
+
+const mapDispatch = {
+  getTaskList: tasksAction.getTaskList,
+  updateTask: tasksAction.updateTask,
+  deleteTask: tasksAction.deleteTask,
+  createTask: tasksAction.createTask,
+};
+
+const mapState = state => {
+  return {
+    tasks: sortedTasksListSelector(state),
+  };
+};
+
+export default connect(mapState, mapDispatch)(TasksList);
